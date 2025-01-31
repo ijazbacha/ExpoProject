86f6974f7476848edab502a87230eb9f852f1125
Ijaz Bacha <76259605+ijazbacha@users.noreply.github.com>
2025-01-31 17:11:14 +0500

    start the design of sign-in screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import images from '../../constants/images'
+import FormField from '../../components/FormField'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image source={images.logo} className="w-[115px] h-[35px]" resizeMode='contain' />
+          <Text className="text-2xl text-white font-psemibold mt-10">Login in to Aora</Text>
+          <FormField />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
