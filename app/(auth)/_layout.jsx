98448dfaaf93f2672ff73a5e9c826f12b9747a01
Ijaz Bacha <76259605+ijazbacha@users.noreply.github.com>
2025-01-31 14:34:16 +0500

    Update _layout.jsx

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 const AuthLayout = () => {
   return (
@@ -9,6 +10,7 @@ const AuthLayout = () => {
       <Stack.Screen name='sign-in' />
       <Stack.Screen name='sign-up' />
     </Stack>
+    <StatusBar backgroundColor='#161622' style='light' />
     </>
   )
 }
